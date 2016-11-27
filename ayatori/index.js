d2df1bfb9d282f85573ae4877bf6ca9017796fa3
makomsh <matsui.mako@g.wakayama-u.jp>
2016-11-27 14:07:32 +0900

    学生用のマイページ変更
    選んだ名刺を青枠で表示
    更新ボタンを押すとあなたの名刺の画像変更
    交換ボタンを押すと
    近づけてください→（５秒後）交換中→（１０秒後）名刺表示＋あやとり点灯→（５秒後）あやとり消灯

--- ayatori/index.js
+++ ayatori/index.js
@@ -1,6 +1,7 @@
+
 var Page = function(){
     //点灯させる
-    this.vincluLed = new VincluLed(100,100);
+	this.vincluLed = new VincluLed(100,100);
    
     //激しく点滅させる
     // this.vincluLed = new VincluLed(100, 10);
@@ -11,18 +12,105 @@ var Page = function(){
         $("#btnStop1").click( $.proxy(this.clickBtnStop,this) );
         $("#btnStop2").click( $.proxy(this.clickBtnStop,this) );
         $("#btnStop3").click( $.proxy(this.clickBtnStop,this) );
+
     };
     
     //点灯 ボタン
     this.clickBtnStart = function(){
         this.vincluLed.on();
     };
+    
     this.clickBtnStop = function(){
         this.vincluLed.off();  
-    }
+    } 
 };
 
 var p = new Page();
 $(function(){
     p.init();
 });
+
+
+this.vincluLed = new VincluLed(100, 10);
+
+
+function selectMeishi(obj){
+
+	elements = document.getElementsByClassName('meishiList');
+
+	/*名刺候補のmselect要素をfalseに*/
+	/*すべての名刺画像の枠線をを初期設定に戻す*/
+    for (var i = 0, len = elements.length; i < len; i++) {
+    	elements[i].setAttribute('mselect','false');
+    	elements[i].setAttribute('style','border:solid 1px #000000');
+	}
+	/*選ばれた名刺のmselect要素をtrueに*/
+	obj.setAttribute('mselect','true');
+	
+	/*選ばれた名刺の枠の設定を変更*/
+	obj.setAttribute('style','border:solid 3px #0000FF');
+
+}
+
+
+$("#mChange").click(function(){
+    
+    meishi = document.getElementById('myMeishi').getAttribute('src');
+    
+    elements = document.getElementsByClassName('meishiList');
+
+    for (var i = 0, len = elements.length; i < len; i++) {
+    	state = elements[i].getAttribute('mselect');
+    	if(state == "true"){
+    		img = elements[i].getAttribute('src');
+    		document.getElementById('myMeishi').setAttribute('src',img);
+    		
+    		break;
+    	}
+	}
+	
+	
+	/*selectの時に変更した応訴を元に戻す*/
+	for (var i = 0, len = elements.length; i < len; i++) {
+    	elements[i].setAttribute('mselect','false');
+    	elements[i].setAttribute('style','border:solid 1px #000000');
+	}
+	
+	
+/*	
+	setTimeout(function(){
+		this.vincluLed = new VincluLed(100, 10);
+     	document.getElementById('myMeishi').setAttribute('src',meishi);
+     	 this.vincluLed.on();
+	},5000);
+*/
+    
+});
+
+
+
+$("#mKokan").click(function(){
+
+	document.getElementById('kokan2').setAttribute('style','width:100%;display:none;');
+    document.getElementById('kokan0').setAttribute('style','width:100%;display:block;');
+    
+	setTimeout(function(){
+     	document.getElementById('kokan0').setAttribute('style','width:100%;display:none;');
+     	document.getElementById('kokan1').setAttribute('style','width:100%;display:block;');
+	},5000);
+
+	setTimeout(function(){
+		document.getElementById('kokan1').setAttribute('style','width:100%;display:none;');
+     	document.getElementById('kokan2').setAttribute('style','width:100%;display:block;');
+    	this.vincluLed.on();
+	},15000);
+
+	setTimeout(function(){
+    	this.vincluLed.off();
+	},20000);
+
+		
+
+});
+
+
