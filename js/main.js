e1f6fe1501fdf4dc29c221b8ee1bb8fa832f28d2
makomsh <matsui.mako@g.wakayama-u.jp>
2016-11-28 19:20:52 +0900

    マイページからトップページへの移動部分修正

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
+$(function(){
+    //変数argはオブジェクトですよ
+	var arg = new Object;
+
+	// 変数pairにURLの?の後ろを&で区切ったものを配列にして代入
+	var pair=location.search.substring(1).split('&');
+	    // location.search.substring(1)は、URLから最初の1文字 (?記号) を除いた文字列を取得する
+	    // .split('&')は&で区切り配列に分割する
+
+
+	// for文でrairがある限りループさせる
+	for(var i=0;pair[i];i++) {
+		// 変数kvにpairを=で区切り配列に分割する
+	    var kv = pair[i].split('=');// kvはkey-value
+		
+		// 最初に定義したオブジェクトargに連想配列として格納
+	    arg[kv[0]]=kv[1];  // kv[0]がkey,kv[1]がvalue
+	}
+	
+	
+			console.log(arg.id);
+			var uId = arg.id;
+	
+	if (arg.id != null){
+		userId = arg.id;
+		console.log(arg.id);
+	    console.log(userId);
+	    elements = document.getElementsByClassName(userId);
+	    
+//	    for (i = 0, len = elements.length; i < len; i++) {
+//	    	elements[i].setAttribute('style','display:block;');
+//		}
+
+		if (userId=="kigyo"){
+			$('#false-parameter').hide();
+		    $('#user_id').hide();
+		    $('#password').hide();
+		    $('#signin').hide();
+		    $('#signup').hide();
+		    $('#company').show();
+		    $('#mypage').show();
+		    $('#senior_voices').hide();
+		    $('#job_hunting_infomation').hide();
+		    $('.mInfo').show();
+		    
+	        elements = document.getElementsByClassName("mypURL");
+    
+		    for (var i = 0, len = elements.length; i < len; i++) {
+		    	elements[i].setAttribute("href","mypage.html?id=kigyo");
+			}
+			
+			elements = document.getElementById("totop").setAttribute("href","index.html?id=kigyo");
+		    
+		}else{
+		    $('#false-parameter').hide();
+		    $('#user_id').hide();
+		    $('#password').hide();
+		    $('#signin').hide();
+		    $('#signup').hide();
+		    $('#student').show();
+		    $('#mypage').show();
+		    $('#rate_plan').hide();
+		    $('.mInfo').show();
+		    
+		    /*マイページ書き換え*/
+		    elements = document.getElementsByClassName("mypURL");
+		    
+		    for (var i = 0, len = elements.length; i < len; i++) {
+		    	elements[i].setAttribute("href","mypage_gaku.html?id=gakusei");
+			}		
+
+			elements = document.getElementById("totop").setAttribute("href","index.html?id=gakusei");
+
+		}
+	}	
+});
+
 
 <!-- 学生用と企業用を切り替える(Javascript)-->
 function init(){
@@ -14,6 +91,8 @@ function init(){
   $('#job_hunting_infomation').show();
   $('.mInfo').hide();
 }
+
+
 
 function judge(){
   var userId = document.getElementById("user_id").value;
